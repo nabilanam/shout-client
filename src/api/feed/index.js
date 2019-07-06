27009885599b35f9dd739b38a1367d40a432d686
Nabil Anam <nabilcoding@gmail.com>
2019-07-06 16:26:26 +0600

    update api endpoints

--- src/api/feed/index.js
+++ src/api/feed/index.js
@@ -5,25 +5,26 @@ export const getOnePost = postId => axios.get(`${route}/${postId}`)
 
 export const getPaginatedPosts = page => axios.get(`${route}/all/${page}`)
 
-export const getPaginatedPostsByUserId = (userId, page) =>
-  axios.get(`${route}/user/${userId}/${page}`)
+export const getPaginatedPostsByUsername = (username, page) =>
+  axios.get(`${route}/${username}/${page}`)
 
-export const addPost = text => axios.post(route, text)
+export const addPost = text => axios.post(route, { text })
 
 export const updatePost = (postId, text) =>
-  axios.post(`${route}/${postId}`, text)
+  axios.put(`${route}/${postId}`, { text })
 
 export const deletePost = postId => axios.delete(`${route}/${postId}`)
 
-export const toggleLike = postId => axios.put(`${route}/${postId}/like`)
+export const like = postId => axios.put(`${route}/${postId}/like`)
 
 export const getPaginatedLikes = (postId, page) =>
   axios.get(`${route}/${postId}/likes/${page}`)
 
-export const addComment = postId => axios.post(`${route}/${postId}/comment`)
+export const addComment = (postId, text) =>
+  axios.post(`${route}/${postId}/comment`, { text })
 
 export const updateComment = (postId, commentId, text) =>
-  axios.put(`${route}/${postId}/comment/${commentId}`, text)
+  axios.put(`${route}/${postId}/comment/${commentId}`, { text })
 
 export const deleteComment = (postId, commentId) =>
   axios.delete(`${route}/${postId}/comment/${commentId}`)
--- src/api/profiles/index.js
+++ src/api/profiles/index.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
 const route = '/api/profiles'
 
-export const getPicture = userId => axios.get(`${route}/${userId}/picture`)
+export const getPicture = userId =>
+  axios
+    .get(`${route}/${userId}/picture`, { responseType: 'arraybuffer' })
+    .then(
+      res =>
+        `data:${res.headers['content-type']};base64,${btoa(
+          String.fromCharCode(...new Uint8Array(res.data))
+        )}`
+    )
 
 export const getProfileByUserId = userId => axios.get(`${route}/id/${userId}`)
 
@@ -12,3 +20,19 @@ export const updateProfile = formData =>
   axios.post(`${route}/update`, formData, {
     headers: { 'Content-Type': 'multipart/form-data' }
   })
+
+export const getProfileWithToken = (userId, token) =>
+  axios.get(`${route}/id/${userId}`, { headers: { 'x-auth-token': token } })
+
+export const getPictureWithToken = (userId, token) =>
+  axios
+    .get(`${route}/${userId}/picture`, {
+      responseType: 'arraybuffer',
+      headers: { 'x-auth-token': token }
+    })
+    .then(
+      res =>
+        `data:${res.headers['content-type']};base64,${btoa(
+          String.fromCharCode(...new Uint8Array(res.data))
+        )}`
+    )
