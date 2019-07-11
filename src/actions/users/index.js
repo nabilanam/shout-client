72a356baed01142b901f7012b8135acb915d9324
Nabil Anam <nabilcoding@gmail.com>
2019-07-11 20:08:07 +0600

    rename fetchUserData to fetchUserProfile in users action

--- src/actions/users/index.js
+++ src/actions/users/index.js
@@ -1,15 +1,17 @@
 import { ADD_USER_DATA, ADD_PROFILE_PICTURE } from './types'
 import * as api from '../../api/profiles'
+import { userNormalized } from '../../schema'
 
 export const fetchUserPicture = userId => dispatch =>
   api.getPicture(userId).then(picture => {
     dispatch({ type: ADD_PROFILE_PICTURE, payload: { userId, picture } })
   })
 
-export const fetchUserData = userId => dispatch =>
-  api.getProfileByUserId(userId).then(res => {
-    const userData = res.data.data
-    api.getPicture(userId).then(picture => {
+export const fetchUserProfile = username => dispatch =>
+  api.getProfileByUsername(username).then(res => {
+    const normalized = userNormalized(res.data.data)
+    const userData = normalized.entities.users[normalized.result]
+    api.getPicture(userData._id).then(picture => {
       dispatch({ type: ADD_USER_DATA, payload: { ...userData, picture } })
     })
   })
