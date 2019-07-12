72db11027229127f242670bbfd81141b40020924
Nabil Anam <nabilcoding@gmail.com>
2019-07-12 14:59:43 +0600

    handle update user in currentUser reducer

--- src/reducers/currentUser/index.js
+++ src/reducers/currentUser/index.js
@@ -1,7 +1,10 @@
 import {
   USER_LOGGED_IN,
   REFRESHING_TOKEN,
-  REMOVE_TOKEN
+  REMOVE_TOKEN,
+  UPDATE_USER,
+  UPDATE_USER_DONE,
+  UPDATE_USER_ERROR
 } from '../../actions/currentUser/types'
 
 const init = {
@@ -13,11 +16,17 @@ const reducer = (state = init, action) => {
   const { type, payload } = action
   switch (type) {
   case USER_LOGGED_IN:
+  case UPDATE_USER_DONE:
     return {
       ...state,
       ...payload,
-      isRefreshingToken: false
+      isRefreshingToken: false,
+      isUpdating: false
     }
+  case UPDATE_USER:
+    return { ...state, isUpdating: true }
+  case UPDATE_USER_ERROR:
+    return { ...state, isUpdating: false }
   case REFRESHING_TOKEN:
     return { ...state, isRefreshingToken: true }
   case REMOVE_TOKEN:
