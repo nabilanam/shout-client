614810853ae9b984c3ef19d0d3e64e377b86b1da
Nabil Anam <nabilcoding@gmail.com>
2019-07-12 21:59:21 +0600

    use componentDidMount instead of componentWillMount in Hero

--- src/components/presentational/Hero/index.js
+++ src/components/presentational/Hero/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 
 class Hero extends Component {
-  componentWillMount() {
+  componentDidMount() {
     document.body.classList.remove('has-navbar-fixed-top')
   }
 
